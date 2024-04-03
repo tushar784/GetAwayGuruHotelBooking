d8d583ad513f9ad2ad08e961497f27fe31b9cba3
tushar784 <survetushar1501@gmail.com>
2024-04-03 23:53:49 +0530

    authcode

--- NestAway-bookings/src/AuthContext/Auth_Context.jsx
+++ NestAway-bookings/src/AuthContext/Auth_Context.jsx
@@ -1,8 +1,23 @@
-import { useContext, createContext } from "react";
+import { data } from "autoprefixer";
+import { useContext, createContext, useState } from "react";
 const AuthContext = createContext()
 
 const AuthProvider = ({children})=>{
-    return <AuthContext.Provider>{children}</AuthContext.Provider>
+    const [isAuthenticate, setAuthenticate] = useState(null)
+    const [user,setUser] = useState(null)
+    const [token,setToken] = useState(null)
+
+    const login = async (data)=>{
+        setAuthenticate(true)
+        setUser(data.user)
+        setToken(data.token0)
+    };
+    const logout = () => {
+        setAuthenticate(false);
+        setUser(null);
+        setToken(null); 
+    };
+    
 }
 
 export default AuthProvider;
