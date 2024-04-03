55fc85900cb4cb2fed0ab08adb99c1592106a227
tushar784 <survetushar1501@gmail.com>
2024-04-03 23:00:18 +0530

    auth

---
+++ NestAway-bookings/src/AuthContext/Auth_Context.jsx
@@ -0,0 +1,11 @@
+import { useContext, createContext } from "react";
+const AuthContext = createContext()
+
+const AuthProvider = ({children})=>{
+    return <AuthContext.Provider>{children}</AuthContext.Provider>
+}
+
+export default AuthProvider;
+export const useAuth = () =>{
+    return useContext(AuthContext)
+}
