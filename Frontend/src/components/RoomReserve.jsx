42547b83502ef862e5dcb976a0bd181ef106493b
tushar784 <survetushar1501@gmail.com>
2024-04-29 23:56:47 +0530

    hello

--- Frontend/src/components/RoomReserve.jsx
+++ Frontend/src/components/RoomReserve.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const RoomReserve = ({ roomPrice, quantity, handleIncrement, handleDecrement }) => {
-//   const totalPrice = roomPrice * quantity;
-  const totalPrice = roomPrice * quantity;
+const RoomReserve = ({ roomPrice }) => {
+
+  
+
   return (
     <div className="border shadow-lg h-[19rem] p-5 mt-14 w-[25rem]">
       <p className="text-black mt-3 text-xl mb-2 font-extrabold">{roomPrice}</p>
-      <p className="mt-5">per night of 1 room</p>
+      <p className="mt-5">per night for 1 room</p>
       <div className="flex items-center mt-4">
         <button onClick={handleDecrement}>-</button>
         <span className="mx-2">{quantity}</span>
