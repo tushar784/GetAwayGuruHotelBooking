5ec0504eae0bf00880b21742eb6d1cf9983f9bbd
tushar784 <survetushar1501@gmail.com>
2024-05-23 18:06:28 +0530

    deployed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,13 @@
-
+import { Suspense } from "react";
 import Routing from "./Routes/Routing";
+import Loading from "./components/Loading";
 
 function App() {
   return (
     <>
+    <Suspense fallback={Loading}>
       <Routing />
+      </Suspense>
     </>
       
   );
--- Frontend/src/Pages/Hotels.jsx
+++ Frontend/src/Pages/Hotels.jsx
@@ -6,6 +6,7 @@ import Layout from "../components/Layout";
 import HotelList from "../components/HotelList";
 import Filters from "../components/Filters";
 import axios from "axios";
+import Loading from "../components/Loading"; // Import the Loading component
 
 const Hotels = () => {
   const { selectedLocation } = useParams();
@@ -14,6 +15,7 @@ const Hotels = () => {
   const [endDate, setEndDate] = useState(null);
   const [guests, setGuests] = useState(1);
   const [rooms, setRooms] = useState(1);
+  const [loading, setLoading] = useState(true); // Add loading state
 
   useEffect(() => {
     // Retrieve values from local storage on mount
@@ -33,6 +35,7 @@ const Hotels = () => {
   useEffect(() => {
     const fetchHotels = async () => {
       try {
+        setLoading(true); // Set loading to true before fetching data
         if (selectedLocation) {
           const url = import.meta.env.VITE_BASE_URL;
           const response = await axios.get(`${url}/api/hotels/location/${selectedLocation}`, {
@@ -44,9 +47,11 @@ const Hotels = () => {
             }
           });
           setHotels(response.data);
+          setLoading(false); // Set loading to false after fetching data
         }
       } catch (error) {
         console.error("Error fetching hotels:", error);
+        setLoading(false); // Also set loading to false in case of an error
       }
     };
 
@@ -57,6 +62,10 @@ const Hotels = () => {
   const handleFiltersChange = (filteredHotels) => {
     setHotels(filteredHotels);
   };
+
+  if (loading) {
+    return <Loading />; // Show the loading spinner while data is being fetched
+  }
 
   return (
     <>
@@ -73,7 +82,6 @@ const Hotels = () => {
 };
 
 export default Hotels;
-
 
 
 
