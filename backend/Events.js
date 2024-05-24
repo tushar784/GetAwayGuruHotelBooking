e4f64a26429069d36025a0351de5a8eca5db9947
tushar784 <survetushar1501@gmail.com>
2024-05-24 11:04:34 +0530

    events api done

--- backend/Events.js
+++ backend/Events.js
@@ -25,4 +25,47 @@ app.get("/events/location/:Location", async (req, res) => {
   }
 });
 
+// Route to get events by category
+app.get("/events/category/:category", async (req, res) => {
+  const { category } = req.params;
+  try {
+    const categoryTitleCase =
+      category.charAt(0).toUpperCase() + category.slice(1).toLowerCase();
+    const events = await Events.find({
+      Event_Category: { $regex: new RegExp("^" + categoryTitleCase, "i") },
+    });
+
+    if (!events || events.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No events found for the specified category" });
+    }
+    res.status(200).json(events);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching events");
+  }
+});
+
+// Route to get events by name
+app.get("/events/name/:eventName", async (req, res) => {
+  const { eventName } = req.params;
+  try {
+    const eventNameRegex = new RegExp(eventName, "i");
+    const events = await Events.find({
+      Event_Name: { $regex: eventNameRegex },
+    });
+
+    if (!events || events.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No events found for the specified name" });
+    }
+    res.status(200).json(events);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching events");
+  }
+});
+
 module.exports = app;
