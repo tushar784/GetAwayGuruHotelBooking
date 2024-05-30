580360a851e5e0f2b84436299572332f494f446b
tushar784 <survetushar1501@gmail.com>
2024-05-30 12:54:53 +0530

    donee

--- backend/server.js
+++ backend/server.js
@@ -95,9 +95,9 @@ app.post('/api/order/validateeventorder', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
 
-// app.listen();
+app.listen();
 
